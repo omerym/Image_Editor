9bdc903651deed15ce275c94848e7b3ff1d59258
Omer Yassir <omerym10@gmail.com>
2023-10-03 23:32:30 +0300

    Add project files.

---
+++ Assignment/Assignment.cpp
@@ -0,0 +1,97 @@
+﻿#include "bmplib.cpp"
+#include <iostream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	// TODO: change welcome message
+	cout << "welcome message:\n";
+	cout << "Enter filename: ";
+	string filename;
+	cin >> filename;
+	bool running = true;
+	while (running)
+	{
+		cout << "Please select a filter to apply or 0 to exit:\n";
+		cout << "1-	Black & White Filter\n"
+			<< "2-	Invert Filter\n"
+			<< "3-	Merge Filter\n"
+			<< "4-	Flip Image\n"
+			<< "5-	Darken and Lighten Image\n"
+			<< "6-	Rotate Image\n"
+			<< "7-	Detect Image Edges\n"
+			<< "8-	Enlarge Image\n"
+			<< "9-	Shrink Image\n"
+			<< "a-	Mirror 1/2 Image\n"
+			<< "b-	Shuffle Image\n"
+			<< "c-	Blur Image\n"
+			<< "d-	Crop Image\n"
+			<< "e-	Skew Image Right\n"
+			<< "f-	Skew Image Up\n"
+			<< "s-      Save the image to a file\n"
+			<< "0-	Exit\n";
+		char operation_code;
+		cin >> operation_code;
+		switch (operation_code)
+		{
+		case '0':
+			running = false;
+			break;
+		case '1':
+			cout << "Work in progress\n";
+			break;
+		case '2':
+			cout << "Work in progress\n";
+			break;
+		case '3':
+			cout << "Work in progress\n";
+			break;
+		case '4':
+			cout << "Work in progress\n";
+			break;
+		case '5':
+			cout << "Work in progress\n";
+			break;
+		case '6':
+			cout << "Work in progress\n";
+			break;
+		case '7':
+			cout << "Work in progress\n";
+			break;
+		case '8':
+			cout << "Work in progress\n";
+			break;
+		case '9':
+			cout << "Work in progress\n";
+			break;
+		case 'a':
+			cout << "Work in progress\n";
+			break;
+		case 'b':
+			cout << "Work in progress\n";
+			break;
+		case 'c':
+			cout << "Work in progress\n";
+			break;
+		case 'd':
+			cout << "Work in progress\n";
+			break;
+		case 'e':
+			cout << "Work in progress\n";
+			break;
+		case 'f':
+			cout << "Work in progress\n";
+			break;
+		case 's':
+			cout << "Work in progress\n";
+			break;
+
+		default:
+			cout << "Invalid input!\n";
+			break;
+		}
+	}
+	return 0;
+}
---
+++ Assignment/bmplib.h
@@ -0,0 +1,21 @@
+#ifndef BMPLIB_H
+#define BMPLIB_H
+
+const int SIZE = 256;
+const int RGB = 3;
+
+// read full-color image from the file specified by filename, into inputImage
+int readRGBBMP(const char* filename, unsigned char inputImage[][SIZE][RGB]);
+
+// write full-color image to the file specified by filename, from outputImage
+int writeRGBBMP(const char* filename, unsigned char outputImage[][SIZE][RGB]);
+
+
+// read grayscale image from the file specified by filename, into inputImage
+int readGSBMP(const char* filename, unsigned char image[][SIZE]);
+
+// write grayscale image to the file specified by filename, from outputImage
+int writeGSBMP(const char* filename, unsigned char outputImage[][SIZE]);
+
+
+#endif
